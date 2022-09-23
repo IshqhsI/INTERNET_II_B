e6292ee03f6e0e38b026b1ce935b2e2958c67751
Muhammad Ridhwan <107637984+IshqhsI@users.noreply.github.com>
2022-09-23 18:20:18 +0700

    Ubah Get jadi Post

--- Form Handling/praktikum1.php
+++ Form Handling/praktikum1.php
@@ -9,7 +9,7 @@
 <body>
     <h1>PHP PRAKTIKUM 1</h1>
 
-    <form action="" method="get">
+    <form action="" method="post">
         <label for="">Nama Lengkap</label> <br>
         <input type="text" name="namaLengkap" placeholder="Masukan Nama Anda"> <br>
 
@@ -31,22 +31,24 @@
     <br>
 
     <?php
-        if (@$_GET['namaLengkap']) {
-            $nama = @$_GET['namaLengkap']; // untuk mengambil nilai dari method GET
+        // @ berfungsi menyembunyikan error karna variabel belum di set/diberi nilai
+        if (@$_POST['namaLengkap']) {
+            $nama = @$_POST['namaLengkap']; // untuk mengambil nilai dari method POST
             echo "Nama : $nama <br>"; // ini untuk menampilkan nilai variable (string)
         }
         
-        if (@$_GET['tempatLahir']) {
-            $tempatLahir  = @$_GET['tempatLahir']; 
-            $tanggalLahir = @$_GET['tanggalLahir']; 
+        if (@$_POST['tempatLahir']) {
+            $tempatLahir  = @$_POST['tempatLahir']; 
+            $tanggalLahir = @$_POST['tanggalLahir']; 
             echo "TTL : $tempatLahir, $tanggalLahir <br>"; 
         }
         
-        if (@$_GET['alamatTinggal']) {
-            $alamat = $_GET['alamatTinggal']; 
+        if (@$_POST['alamatTinggal']) {
+            $alamat = @$_POST['alamatTinggal']; 
             echo "Alamat : $alamat <br>";
         }
     ?>
+    
 
 </body>
 </html>
